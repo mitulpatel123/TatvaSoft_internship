fc61cbaf5c59d3fdb214c00d3b5c157c7b64bdf6
mitul <mitulpatel12180@gmail.com>
2023-05-24 13:25:45 +0530

    day5

--- my-app/src/components/Apple.js
+++ my-app/src/components/Apple.js
@@ -1,115 +1,174 @@
 import { useNavigate } from "react-router-dom";
-import Button from '@mui/material/Button';
-import { useState } from "react";
-import { TextField } from "@mui/material";
-import { Popover } from "@mui/material";
-import { Avatar } from "@mui/material";
-import LogoutIcon from '@mui/icons-material/Logout';
+
+import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import { Avatar, Popover } from "@mui/material";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 export const Apple = () => {
-    const [name, setName] = useState("mitul");
-    const [email, setEmail] = useState("mitul@gmail.com");
-    const [open, setOpen] = useState(false);
-    const [anchorE1, setAnchorE1] = useState(null);
-    const Navigate = useNavigate();
-    
-    const onHomePageButtonClick = () => {
-        Navigate("/");
-        console.log("Name: ", name);
-        console.log("Email: ", email);
-        // alert("Button has been click");
-    };
+  // const [name, setName] = useState();
+  // const [email, setEmail] = useState();
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const Navigate = useNavigate();
 
-    const handleClick = (event) => {
-        console.log(123);
-        setAnchorE1(event.currentTarget);
-        setOpen(true);
-    }
+  useEffect(() => {
+    // console.log("The new value of Name : ", name);
+    // return () => {
+    //   console.log("The old value of Name : ", name);
+    // };
+  }, []);
 
-    const handleClose = () => {
-        setAnchorE1(null);
-        setOpen(false);
-    }
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().min(
+      3,
+      "Please make sure you have entered you name with atleast 3 char."
+    ),
+    email: Yup.string().email("Please enter a valid email address"),
+  });
 
-    return (
-        <div style={{
-            padding: 15,
-        }}>
-            <div style={{
-                display: "flex",
-                justifyContent: "flex-end",
-            }}
+  const initialValues = {
+    name: "",
+    email: "",
+  };
 
-            >
-                <div onClick={handleClick}
-                    style={{
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center",
-                        columnGap: "5px",
-                    }}>
-                    <Avatar sx={{ bgcolor: "blue" }}>MP</Avatar>
-                </div>
-            </div>
-            <div style={{
-                padding: 5,
-                display: "flex",
-                flexDirection: "column",
-                rowGap: 8
+  const onFormSubmit = (values) => {
+    console.log("On the form submitted", values);
+    alert("Form Submmited");
+  };
 
-            }}>
-                {/* <div style={{
-                    padding: 5
-                }}>Apple</div> */}
+  const handleClick = (event) => {
+    console.log(123);
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
 
-                <TextField
-                    variant="outlined"
-                    type="text"
-                    label="Name"
-                    value={name}
-                    placeholder="Name"
-                    onChange={(e) => {
-                        setName(e.target.value);
-                    }}>
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
 
-                </TextField>
-                <TextField
-                    variant="outlined"
-                    label="Email"
-                    type="email"
-                    value={email}
-                    placeholder="email"
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                    }}
-                ></TextField>
-
-                <Button variant="contained" onClick={onHomePageButtonClick}>Submit</Button>
-
-            </div>
-            <Popover
-                open={open}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                anchorEl={anchorE1}
-                onClose={handleClose}
-            >
-                <div style={{
-                    padding: 5
-                }}>
-                    <h5>Mitul Patel</h5>
-                    <LogoutIcon onClick={onHomePageButtonClick} />
-                </div>
-            </Popover>
-
-
+  return (
+    <div
+      style={{
+        padding: 5,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          cursor: "pointer",
+        }}
+      >
+        <div
+          onClick={handleClick}
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            columnGap: 5,
+            cursor:"pointer",
+          }}
+        >
+          <Avatar sx={{ bgcolor: "blue" }}>MB</Avatar>
         </div>
+      </div>
+      <div
+        style={{
+          padding: 5,
+          display: "flex",
+          flexDirection: "column",
+          rowGap: 8,
+        }}
+      >
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onFormSubmit}
+        >
+          {({
+            value,
+            errors,
+            touched,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: 5,
+                }}
+              >
+                <TextField
+                  variant="outlined"
+                  type="text"
+                  label="Name"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {touched.name && (
+                  <span
+                    style={{
+                      padding: 5,
+                      color: "red",
+                      fontSize: 16,
+                      fontWeight: 500,
+                    }}
+                  >
+                    {errors.name}
+                  </span>
+                )}
+              </div>
 
-    )
-}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: 5,
+                }}
+              >
+                <TextField
+                  variant="outlined"
+                  type="email"
+                  label="Email"
+                  id="email"
+                  name="email"
+                  placeholder="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {touched.email && (
+                  <span
+                    style={{
+                      padding: 5,
+                      color: "red",
+                      fontSize: 16,
+                      fontWeight: 500,
+                    }}
+                  >
+                    {errors.email}
+                  </span>
+                )}
+              </div>
+              <Button variant="contained" type="submit" className="">
+                Submit
+              </Button>
+            </form>
+          )}
+        </Formik>
+      </div>
+      
+    </div>
+  );
+};
